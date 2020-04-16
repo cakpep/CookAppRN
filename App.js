e5270ba89c34842084ee4b62dca9b2f0f7fa9cb2
febrimaschut@gmail.com <febrimaschut@gmail.com>
2020-04-16 15:02:36 +0700

    simplify App root and components management

--- App.js
+++ App.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 
 import configureStore from './src/store';
 
-import PlaceScreen from './src/components/place/PlaceScreen';
+import MainApp from './src/components/MainApp';
 
 export default function App() {
   return (
     <Provider store={ configureStore() }>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <PlaceScreen/>        
-      </View>
+      <MainApp/>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
