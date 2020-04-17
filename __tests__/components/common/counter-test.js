d13ff315d764afe2468f8b14b9c35bec59876181
febrimaschut@gmail.com <febrimaschut@gmail.com>
2020-04-17 11:41:48 +0700

    add unit test using jest

---
+++ __tests__/components/common/counter-test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import renderer from 'react-test-renderer';
+
+import { MonoText } from '../../../src/components/common/StyledText';
+
+test('render with styles', () => {
+    
+    const styles = StyleSheet.create({
+        textHighlightContainer: {
+            backgroundColor: '#fff',
+            borderRadius: 3,
+            padding: 4,
+        }
+    })
+   
+    const tree = renderer.create(<MonoText style = {styles.textHighlightContainer}> Test </MonoText>).toJSON()
+    expect(tree).toMatchSnapshot()
+})
+
+test('render with no styles', () => {
+    const tree = renderer.create(<MonoText> No Styles </MonoText>).toJSON()
+    expect(tree).toMatchSnapshot()
+})
