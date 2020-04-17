5857854712ebc55d9906f27f2c7efb682ac99252
febrimaschut@gmail.com <febrimaschut@gmail.com>
2020-04-17 16:48:17 +0700

    add stack navigator

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -4,17 +4,24 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import BottomTabNavigator from './navigation/BottomTabNavigator'
+import PlaceScreen from './place/PlaceScreen'
+
+const Stack = createStackNavigator()
+
+const CookStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Root" component={BottomTabNavigator} />
+      <Stack.Screen name="Place" component={PlaceScreen} />
+    </Stack.Navigator>
+  );
+}
 
 export default function MainApp() {
- 
-  const Stack = createStackNavigator()
-
   return (
     <View style={styles.container}>
         <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
+          <CookStackNavigator/>
         </NavigationContainer>
       </View>
   )
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,7 @@
-//This is an example code for Bottom Navigation//
-import React from 'react';
-//import react in our code.
+import React, { Component } from 'react'
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
-//import all the basic component we have used
 
-export default class HomeScreen extends React.Component {
-  //Home Screen to show in Home Option
+export default class HomeScreen extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
