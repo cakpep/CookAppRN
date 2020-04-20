8086161c31d4fde113de2ab92e13cc68148ec553
febrimaschut@gmail.com <febrimaschut@gmail.com>
2020-04-20 11:29:57 +0700

    move stack screen into nav dir

---
+++ src/components/navigation/StackScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import BottomTabNavigator from '../navigation/BottomTabNavigator'
+import PlaceScreen from '../place/PlaceScreen'
+import AlphabetsScreen from '../alphabets/alphabets'
+
+const Stack = createStackNavigator()
+
+export default () => {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Root" component={BottomTabNavigator} />
+            <Stack.Screen name="Place" component={PlaceScreen} />
+            <Stack.Screen name="Alphabets" component={AlphabetsScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
